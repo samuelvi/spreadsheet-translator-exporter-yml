02d9706e62706335e91776dc3944b95a40166f10
Samuel Vicent <samuelvicent@gmail.com>
2025-10-22 18:41:54 +0200

    Refactored `YmlBuilder` to improve YAML generation logic and updated dependencies in `composer.json`.

--- src/YmlBuilder.php
+++ src/YmlBuilder.php
@@ -13,27 +13,52 @@ namespace Atico\SpreadsheetTranslator\Exporter\Yml;
 
 class YmlBuilder
 {
-    function __construct(private array $data, private string $nameSeparator)
+    function __construct(private $data, private $nameSeparator)
     {
     }
 
     function buildDocument(): string
     {
-        $rows = [];
+        // First, build a tree structure
+        $tree = [];
         foreach ($this->data as $name => $value) {
-
             $keys = explode($this->nameSeparator, (string) $name);
+            $current = &$tree;
 
             foreach ($keys as $index => $key) {
                 $isLastKey = (count($keys) === $index + 1);
-                $padding = ($index === 0)?'':str_pad(' ', $index*4);
-                $rows[] = sprintf('%s%s: %s', $padding, $key, ($isLastKey)?'>':'');
+
                 if ($isLastKey) {
-                    $padding = str_pad(' ', ($index+1)*4);
-                    $rows[] = sprintf('%s%s', $padding, str_replace(PHP_EOL, PHP_EOL . $padding, addslashes((string) $value)));
+                    $current[$key] = $value;
+                } else {
+                    if (!isset($current[$key])) {
+                        $current[$key] = [];
+                    }
+                    $current = &$current[$key];
                 }
             }
         }
+
+        // Then render the tree as YAML
+        return $this->renderTree($tree, 0);
+    }
+
+    private function renderTree(array $tree, int|float $level): string
+    {
+        $rows = [];
+        $padding = str_pad('', $level * 4);
+
+        foreach ($tree as $key => $value) {
+            if (is_array($value)) {
+                $rows[] = sprintf('%s%s:', $padding, $key);
+                $rows[] = $this->renderTree($value, $level + 1);
+            } else {
+                $rows[] = sprintf('%s%s: >', $padding, $key);
+                $valuePadding = str_pad('', ($level + 1) * 4);
+                $rows[] = sprintf('%s%s', $valuePadding, str_replace(PHP_EOL, PHP_EOL . $valuePadding, addslashes((string) $value)));
+            }
+        }
+
         return implode(PHP_EOL, $rows);
     }
 }
